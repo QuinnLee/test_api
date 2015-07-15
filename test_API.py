9e0901ee49b7d816b15c22d40289fae4aeb4c395
Corey Chivers <corey.chivers@uphs.upenn.edu>
2015-07-15 09:35:08 -0400

    crossdomain allow headers

--- test_API.py
+++ test_API.py
@@ -2,26 +2,67 @@
 
 from flask import Flask, jsonify, abort, make_response, request, url_for, current_app, json
 import numpy as np
-import datetime
+from datetime import timedelta, datetime
+from functools import update_wrapper
 
 app = Flask(__name__)
 
 route_root = '/api/v1.0/'
 
+def crossdomain(origin='*', methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+
 def gen_data(now,i):
-    x = {'y_var':np.random.normal()}
-    date = now+datetime.timedelta(days=i)
+    date = now+timedelta(days=i)
     date = date.strftime("%d-%b-%y")
-    x.update({'date':date})
+    x = {'date':date,'y_var':np.random.normal()}
     return x
 
 @app.route(route_root + 'random', methods=['GET'])
+@crossdomain(headers='Content-Type')
 def random_normal():
-    now = datetime.datetime.now()
+    now = datetime.now()
     x = [gen_data(now,i) for i in range(100)]
     x = {'data':x}
     return jsonify(x)
-
 
 @app.route(route_root + 'echo', methods=['POST'])
 def echo():
