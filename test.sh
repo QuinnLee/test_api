e694b2b21de16be9c5c738d1063055f3746c805c
Corey Chivers <corey.chivers@mail.mcgill.ca>
2015-12-14 09:39:57 -0500

    Update test.sh
    add some instructions.

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Post some data to the 'echo' endpoint. This should return the same data back in response.
 curl -H "Content-Type: application/json" -X POST -d '{"foo":"bar"}' localhost:5000/api/v1.0/echo
 
+# Get some data from the 'random' endpoint
 curl -H "Content-Type: application/json" -X GET localhost:5000/api/v1.0/random
